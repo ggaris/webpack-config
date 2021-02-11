432b99581e7bda126432d7b04397629e669972a6
jiantong <jiantong12138.com>
2021-02-11 16:19:53 +0800

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
-  mode: "development",
-  devtool: false,
+  mode: mode,
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
   devServer: {
     contentBase: "./dist",
   },
